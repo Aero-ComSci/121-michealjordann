54edacb3928c59b2c3b710f96b925e5f082586a4
michealjordann <148394954+michealjordann@users.noreply.github.com>
2024-10-24 14:31:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ font_setup = ("Arial", 20, "normal")
 timer = 30
 counter_interval = 1000   #1000 represents 1 second
 timer_up = False
+play = False
 
 
 #-----initialize turtle-----
@@ -24,60 +25,68 @@ counter.speed(0)
 scwriter.speed(0)
 t.speed(0)
 #-----game functions--------
-def spot_clicked(x, y):
-    t.stamp()
-    color = ["red","blue", "green", "orange", "purple","brown","yellow"]
-    shape = ["turtle","classic","triangle","square"]
-    t.shapesize(rand.randint(1,8))
-    t.color(rand.choice(color))
-    t.shape(rand.choice(shape))
-    change_position()
-    counterpos()
-   
-
-
-def change_position():
-    
-    xnew = rand.randint(-360,260)
-    ynew = rand.randint(-360,260)
-    t.penup()
-    t.goto(xnew,ynew)
-    update_score()
-    
-
-def update_score():
-    global score
-    score += 1
-    scwriter.penup()
-    scwriter.goto(0,295)
-    scwriter.clear()
-    scwriter.write("Score:", font=font_setup)
-    scwriter.goto(80,295)
-    scwriter.write(score, font=font_setup)
-
-def counterpos():
-   t.penup()
-   counter.goto(-300,295)
-   
-
-def countdown():
-  global timer, timer_up
-  counter.clear()
-  if timer <= 0:
-    counter.write("Time's Up", font=font_setup)
-    timer_up = True
-    if timer_up == False:
-       update_score()
-       change_position()
+def startgame():
+    yn = input("type yes to start. ")
+    if yn == 'yes':
+        play = True
     else:
-       t.hideturtle()
-       scwriter.hideturtle()
-       counter.hideturtle()
+        print("wont start till you say yes in all lowercase buddy!")
+
+while play == True:
+    def spot_clicked(x, y):
+        t.stamp()
+        color = ["red","blue", "green", "orange", "purple","brown","yellow"]
+        shape = ["turtle","classic","triangle","square"]
+        t.shapesize(rand.randint(1,8))
+        t.color(rand.choice(color))
+        t.shape(rand.choice(shape))
+        change_position()
+        counterpos()
+    
+
+
+    def change_position():
         
-  else:
-    counter.write("Timer: " + str(timer), font=font_setup)
-    timer -= 1
-    counter.getscreen().ontimer(countdown, counter_interval) 
+        xnew = rand.randint(-360,260)
+        ynew = rand.randint(-360,260)
+        t.penup()
+        t.goto(xnew,ynew)
+        update_score()
+        
+
+    def update_score():
+        global score
+        score += 1
+        scwriter.penup()
+        scwriter.goto(0,295)
+        scwriter.clear()
+        scwriter.write("Score:", font=font_setup)
+        scwriter.goto(80,295)
+        scwriter.write(score, font=font_setup)
+
+    def counterpos():
+        t.penup()
+    counter.goto(-300,295)
+    
+
+    def countdown():
+        global timer, timer_up
+    counter.clear()
+    if timer <= 0:
+        counter.write("Time's Up", font=font_setup)
+        timer_up = True
+        if timer_up == False:
+            update_score()
+            change_position()
+        else:
+            t.hideturtle()
+            scwriter.hideturtle()
+            counter.hideturtle()
+            
+    else:
+        counter.write("Timer: " + str(timer), font=font_setup)
+        timer -= 1
+        counter.getscreen().ontimer(countdown, counter_interval) 
     
  
    
